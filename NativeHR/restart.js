238583f50627109fdd31146b24c7a7942a0b952d
Humaid Khan <humaidk2@gmail.com>
2016-12-14 15:50:06 -0800

    Add restart

---
+++ NativeHR/restart.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  Button,
+  Dimensions,
+  TouchableHighlight
+} from 'react-native';
+
+module.exports = (props) => (
+  <View>
+    
+    <View>
+      <View >
+        <TextInput style={{width: 100}} className='newPetName'  onChangeText={(text) => props.getInput(text)} />
+        <Button title="MakeNewPet" onPress={props.newPet} />
+      </View>
+    </View>
+  </View>
+);
