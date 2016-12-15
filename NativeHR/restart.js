d84ff982d5681c620c5876cddecc6789f6d174b4
Chris <krizloo@gmail.com>
2016-12-14 16:02:02 -0800

    Add styling to restart functionality

--- NativeHR/restart.js
+++ NativeHR/restart.js
@@ -12,13 +12,28 @@ import {
 } from 'react-native';
 
 module.exports = (props) => (
-  <View>
-    
-    <View>
-      <View >
-        <TextInput style={{width: 100}} className='newPetName'  onChangeText={(text) => props.getInput(text)} />
-        <Button title="MakeNewPet" onPress={props.newPet} />
-      </View>
+  <View style={styles.buttonsContainer}>
+    <TextInput style={styles.inputContainer} placeholder='Enter new pet name!' className='newPetName' onChangeText={(text) => props.getInput(text)} />
+    <View style={styles.restartButton}>
+      <Button title="Restart" onPress={props.newPet} />
     </View>
   </View>
 );
+
+const styles = StyleSheet.create({
+  buttonsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  inputContainer: {
+    flex: 7,
+    marginLeft: 20,
+    marginBottom: 20,
+  },
+  restartButton: {
+    flex: 3,
+    marginLeft: 10,
+    marginRight: 20,
+    marginBottom: 20,
+  }
+});
