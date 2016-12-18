3d3b72ce234412606f5b13f985fbae35b7db644c
Humaid Khan <humaidk2@gmail.com>
2016-12-17 16:04:28 -0800

    add login/signup componenent

--- NativeHR/index.android.js
+++ NativeHR/index.android.js
@@ -16,11 +16,14 @@ import StatusMessage from './statusMessage.js';
 import Info from './info.js';
 import Restart from './restart.js';
 import Question from './question.js';
+import Login from './login.js';
+import Signup from './signup.js';
+import Petlist from './petlist.js';
 import {Scene, Router} from 'react-native-router-flux';
 import NativeHR2 from './NativeHR2.js';
 import cookingChallenge from './cookingChallenge.js';
 
-
+const refreshOnBack = () => { Actions.pop(); Actions.refresh(); };
 export default class NativeHR extends Component {
   constructor(props) {
     super(props);
@@ -31,10 +34,12 @@ export default class NativeHR extends Component {
     return (
       <Router hideNavBar={true}>
         <Scene key="root">
-          <Scene key="NativeHR2" initial={true} component={NativeHR2} title="Home"/>
-              <Scene key="petBox" component={PetBox} title="Register"/>
-              <Scene key="question" component={Question} title="Question" duration={0}/>
-              <Scene key="cookingChallenge" component={cookingChallenge} title="cookingChallenge" duration={0}/>
+          <Scene key="NativeHR2" component={NativeHR2} title="Home"/>
+          <Scene key="petBox" component={PetBox} title="Register"/>
+          <Scene key="Login" initial={true} component={Login} title="Login" />
+          <Scene key="Signup" component={Signup} title="Signup" />
+          <Scene key="question" component={Question} onBack={refreshOnBack} title="Question" duration={0}/>
+          <Scene key="cookingChallenge" component={cookingChallenge} title="cookingChallenge" duration={0}/>
           </Scene>
       </Router>
     );
